2beb5ba27bbd945c37b3fc6bca803198ded2c279
HnrBrg <hnr.berger@gmail.com>
2022-07-27 18:28:34 +0200

    Save before init

--- execTest.sh
+++
@@ -1,2 +0,0 @@
-https://thegraph.com/docs/en/developer/subgraph-debug-forking/
-https://github.com/graphprotocol/graph-node#running-a-local-graph-node
--- src/mappings/helpers.ts
+++ src/mappings/helpers.ts
@@ -1,7 +1,8 @@
 import { BigInt } from "@graphprotocol/graph-ts";
 
 export const ADDRESS_ZERO = "0x0000000000000000000000000000000000000000";
-export const FACTORY_ADDRESS = "0xff65f4491bc7c0676e96471bfff84205561940be"; // TO LOWERCASE
+// export const FACTORY_ADDRESS = "0xff65f4491bc7c0676e96471bfff84205561940be"; // TO LOWERCASE
+export const FACTORY_ADDRESS = "0xc04ed88c05fcde0f94305598eb14f1624b091a5c"; // TO LOWERCASE // POLYGON
 
 export let ZERO_BI = BigInt.fromI32(0);
 export let ONE_BI = BigInt.fromI32(1);
