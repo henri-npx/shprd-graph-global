ce02dbadbf47c2043c453d5b59ae09577f54e11f
aleksandar-npx <aleksandar@napoleonx.ai>
2022-07-27 16:56:31 +0200

    setup ok

---
+++ execTest.sh
@@ -0,0 +1,2 @@
+https://thegraph.com/docs/en/developer/subgraph-debug-forking/
+https://github.com/graphprotocol/graph-node#running-a-local-graph-node
---
+++ src/mappings/helpers.ts
@@ -0,0 +1,8 @@
+import { BigInt } from "@graphprotocol/graph-ts";
+
+export const ADDRESS_ZERO = "0x0000000000000000000000000000000000000000";
+export const FACTORY_ADDRESS = "0xff65f4491bc7c0676e96471bfff84205561940be"; // TO LOWERCASE
+
+export let ZERO_BI = BigInt.fromI32(0);
+export let ONE_BI = BigInt.fromI32(1);
+export let BI_18 = BigInt.fromI32(18);
